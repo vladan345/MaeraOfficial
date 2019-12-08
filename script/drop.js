dd0e2da6ecf2f7837c1bdaae9718e7170243e49b
vladan345 <57448109+vladan345@users.noreply.github.com>
2019-12-08 17:51:41 +0100

    final

--- script/drop.js
+++
@@ -1,9 +0,0 @@
-var menu = document.getElementById('menu');
-var drop = document.getElementsByClassName('close')[0];
-
-menu.addEventListener('click', otvori, false);
-
-function otvori() {
-    drop.classList.toggle('open');
-    drop.classList.toggle('close');
-}
